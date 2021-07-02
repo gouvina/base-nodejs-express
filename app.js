fe862ac99ebd60c94e20c50a592fe341080eea75
Germán Ouviña <germanouvina@gmail.com>
2021-07-02 17:59:02 -0300

    Bodyparser deprecated

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ const corsOptions = {
 // Express app rules
 const app = express();
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
